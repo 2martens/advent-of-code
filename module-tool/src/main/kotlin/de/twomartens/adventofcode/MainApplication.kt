e9c1af72d732fa1981b5c7a4858a88402e3de840
Jim Martens <github@2martens.de>
2023-12-09 00:20:13 +0100

    refactor: Remove obsolete function

--- module-tool/src/main/kotlin/de/twomartens/adventofcode/MainApplication.kt
+++ module-tool/src/main/kotlin/de/twomartens/adventofcode/MainApplication.kt
@@ -1,16 +1,10 @@
 package de.twomartens.adventofcode
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class MainApplication: CommandLineRunner {
-    override fun run(vararg args: String?) {
-
-//        TODO("Not yet implemented")
-    }
-}
+open class MainApplication
 
 fun main(args: Array<String>) {
     runApplication<MainApplication>(*args)
